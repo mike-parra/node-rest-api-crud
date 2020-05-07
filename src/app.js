b5a1c78093f1306d45ff2fb8a2f61030d531d2bd
Mike <mparra190@gmail.com>
2020-05-07 17:26:07 -0500

    Specific service for upload files

--- src/app.js
+++ src/app.js
@@ -3,30 +3,29 @@ const morgan = require('morgan');// improtando morgan para ser el logger
 const mongoose = require('mongoose')//improtamos mongoose para la conexion a la bd en mongo
 const userRoutes = require('./routes/usersroutes')// rutas del rest
 
-
 //+-+-+-+-+-+- MULTER -+-+-+-+-+-+-+-+
-const multer = require('multer'); // biblioteca que nos permite recibir y enviar archivos meiante los servicios
-const path = require('path');// biblioteca para el destino interno de proyecto
-const { v4: uuidv4 } = require('uuid');// generador aleatorio de id unicos universales para los nombres de los archivos a subir
+// const multer = require('multer'); // biblioteca que nos permite recibir y enviar archivos meiante los servicios
+ const path = require('path');// biblioteca para el destino interno de proyecto
+// const { v4: uuidv4 } = require('uuid');// generador aleatorio de id unicos universales para los nombres de los archivos a subir
 
-const storage = multer.diskStorage({// configuraciones de multer usando la constante storage
-    destination: path.join(__dirname, 'public/documents'),
-    filename: (req, file, next) => {
-        //cb(null,file.originalname) //establece propiedades de guardado, en particlar, guardar con el nombre original
-        next(null, uuidv4() + path.extname(file.originalname)) //concatenamos el id generaro con uuid con la extencion del archivo
-    },
-    limits: { fileSize: 1000000 }, // limites para la subida de los archivos
-    fileFilter: (req, file, next) => { // filtarndo el tipo de archivo
-        const filterTypes = /jpg|jpeg|png|gif|pdf/;
-        const filetype = filterTypes.test(file.mimetype)// comprobando el tipo de archivo
-        const extname = filterTypes.test(path.extname(file.originalname))// comprovando la extencion del archivo
-        if (filetype && extname) {
-            return next(null, true);
-        } else {
-            next('error: File type not supported')
-        }
-    }
-});
+// const storage = multer.diskStorage({// configuraciones de multer usando la constante storage
+//     destination: path.join(__dirname, 'public/documents'),
+//     filename: (req, file, next) => {
+//         //cb(null,file.originalname) //establece propiedades de guardado, en particlar, guardar con el nombre original
+//         next(null, uuidv4() + path.extname(file.originalname)) //concatenamos el id generaro con uuid con la extencion del archivo
+//     },
+//     limits: { fileSize: 1000000 }, // limites para la subida de los archivos
+//     fileFilter: (req, file, next) => { // filtarndo el tipo de archivo
+//         const filterTypes = /jpg|jpeg|png|gif|pdf/;
+//         const filetype = filterTypes.test(file.mimetype)// comprobando el tipo de archivo
+//         const extname = filterTypes.test(path.extname(file.originalname))// comprovando la extencion del archivo
+//         if (filetype && extname) {
+//             return next(null, true);
+//         } else {
+//             next('error: File type not supported')
+//         }
+//     }
+// });
 
 
 const app = express()//inicializando express
@@ -62,7 +61,7 @@ app.use(express.urlencoded({ extended: false }));// soportando los datos JSON a 
 app.use(express.json());// soprtando los datos JSON a recibir se usa en lugar de body-parser
 
 // MULTER
-app.use(multer({storage}).single('file'));//.any());// Improtant, the key on postman must be de same as 'image' or what ever you put in single, you can use array if you are uploading more that 1 file
+//app.use(multer({storage}).single('file'));//.any());// Improtant, the key on postman must be de same as 'image' or what ever you put in single, you can use array if you are uploading more that 1 file
 
 // Rutas de la pp
 app.use('/user', userRoutes)
--- src/routes/usersroutes.js
+++ src/routes/usersroutes.js
@@ -6,6 +6,32 @@ const router = require('express-promise-router')();//este router es mas eficient
 
 const userController = require('../controllers/userController')
 
+//+-+-+-+-+-+- MULTER -+-+-+-+-+-+-+-+
+const multer = require('multer'); // biblioteca que nos permite recibir y enviar archivos meiante los servicios
+const path = require('path');// biblioteca para el destino interno de proyecto
+const { v4: uuidv4 } = require('uuid');// generador aleatorio de id unicos universales para los nombres de los archivos a subir
+
+const storage = multer.diskStorage({// configuraciones de multer usando la constante storage
+    destination: path.join(__dirname, '../public/documents'),
+    filename: (req, file, next) => {
+        //cb(null,file.originalname) //establece propiedades de guardado, en particlar, guardar con el nombre original
+        next(null, uuidv4() + path.extname(file.originalname)) //concatenamos el id generaro con uuid con la extencion del archivo
+    },
+    limits: { fileSize: 1000000 }, // limites para la subida de los archivos
+    fileFilter: (req, file, next) => { // filtarndo el tipo de archivo
+        const filterTypes = /jpg|jpeg|png|gif|pdf/;
+        const filetype = filterTypes.test(file.mimetype)// comprobando el tipo de archivo
+        const extname = filterTypes.test(path.extname(file.originalname))// comprovando la extencion del archivo
+        if (filetype && extname) {
+            return next(null, true);
+        } else {
+            next('error: File type not supported')
+        }
+    }
+});
+
+const upload = multer({storage});
+
 router.get('/', userController.listUsers)//listar usuarios
 router.get('/:userId', userController.getUser);//Buscar un unico usuario, recibiendo el id mediante el endpoint como parametro para eso es el :userIDd
 router.get('/:userId/cars',userController.getUserCars)// obteniendo los carros por usuario
@@ -14,6 +40,6 @@ router.post('/:userId/cars', userController.newUserCar)//insertando carros a un 
 router.put('/:userId', userController.updateUserPut)//actualiar usuario
 router.delete('/:userId',userController.deleteUser)//eliminar un usuario
 
-router.post('/document',userController.documents)//servicio para subir archivos
+router.post('/document',upload.array('documents',10),userController.documents)//servicio para subir archivos
 
 module.exports = router;
